87a9e4bf8a1eb3d60b26cd0a1a9ce53b6bfea3e2
Koushik Roy <koushik@meff.me>
2019-02-27 01:15:32 -0800

    Run black on the codebase

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ EXTREMIST_PROB = 0.08
 EXTREMIST_RADIUS = 0.01
 NORMAL_RADIUS = 0.1
 
-EXTREMIST_SPREAD=0.001
+EXTREMIST_SPREAD = 0.001
+
 
 class Entity:
-    uuid_counter = 0 # is this going to be unique with subclasses?
-
+    uuid_counter = 0  # is this going to be unique with subclasses?
 
     @classmethod
     def _get_uuid(cls):
@@ -31,9 +31,8 @@ class User(Entity):
         self.enthusiasm = enthusiasm
         self.passionate = passionate
 
-
     def vote(self, post):
-        if abs(post.opinion - self.opinion) <= self.opinion_radius*self.opinion:
+        if abs(post.opinion - self.opinion) <= self.opinion_radius * self.opinion:
             return 1
 
         return -1 if self.passionate else 0
@@ -45,7 +44,6 @@ class Post(Entity):
         self.opinion = opinion
         self.score = 0
 
-
     def __eq__(self, other):
         return self.score == other.score
 
@@ -53,7 +51,7 @@ class Post(Entity):
 def update_loop(user, posts):
     # update_loop mutates posts
     post_count = 0
-    processed_posts = SortedList(key=attrgetter('score'))
+    processed_posts = SortedList(key=attrgetter("score"))
 
     for i in range(user.enthusiasm):
         # Pop the highest element on this list
@@ -61,16 +59,15 @@ def update_loop(user, posts):
         post.score += user.vote(post)
         processed_posts.add(post)
 
-
     posts.update(processed_posts)
 
 
 def run(num_users, num_runs):
     extremist_opinion = random()
-    
+
     # Initialize the posts in a sorted list
-    posts = SortedList(key=attrgetter('score'))
-    for i in range(num_users*10):
+    posts = SortedList(key=attrgetter("score"))
+    for i in range(num_users * 10):
         posts.add(Post(random()))
 
     users = []
@@ -78,31 +75,30 @@ def run(num_users, num_runs):
         is_enthusiast = random() < ENTHUSIAST_PROB
         enthusiasm = 100 if is_enthusiast else 10
         passionate = random() < EXTREMIST_PROB
-        opinion = uniform(
-            extremist_opinion - extremist_opinion*EXTREMIST_SPREAD,
-            extremist_opinion + extremist_opinion*EXTREMIST_SPREAD,
-        ) if passionate else random()
+        opinion = (
+            uniform(
+                extremist_opinion - extremist_opinion * EXTREMIST_SPREAD,
+                extremist_opinion + extremist_opinion * EXTREMIST_SPREAD,
+            )
+            if passionate
+            else random()
+        )
         opinion_radius = EXTREMIST_RADIUS if passionate else NORMAL_RADIUS
-        users.append(User(
-            opinion,
-            opinion_radius,
-            enthusiasm,
-            passionate,
-        ))
+        users.append(User(opinion, opinion_radius, enthusiasm, passionate))
 
-    shuffle(users) # shuffle the users so we "randomly" activate them
+    shuffle(users)  # shuffle the users so we "randomly" activate them
 
     for i in range(num_runs):
-        print('Updating:', i+1)
+        print("Updating:", i + 1)
         for u in users:
             update_loop(u, posts)
-        
+
     for i in range(20):
         post = posts.pop()
-        print('Post Score: {} | Opinion: {}'.format(post.score, post.opinion))
+        print("Post Score: {} | Opinion: {}".format(post.score, post.opinion))
 
-    print('Extremist Opinion:', extremist_opinion)
-    print('Num. of Extremist Users:', len([u for u in users if u.passionate]))
+    print("Extremist Opinion:", extremist_opinion)
+    print("Num. of Extremist Users:", len([u for u in users if u.passionate]))
 
 
 def main():
